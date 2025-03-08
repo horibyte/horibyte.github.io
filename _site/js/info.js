b987e626fa33d3492c52738d6463c4e6986ce4dd
Stuffy <158113655+stuffyxp@users.noreply.github.com>
2025-03-08 10:09:35 -0600

    a

--- _site/js/info.js
+++ _site/js/info.js
@@ -2,7 +2,7 @@ var intervalId;
 
 function displayUserAgent() {
   var userAgentString = navigator.userAgent;
-  var htmlContent = '<p><i class="fas fa-circle-info"></i> Horibyte Website Version 2.3.3.3095.main.250308-1005</p><i class="fas fa-user-gear"></i>&nbsp;' + userAgentString;
+  var htmlContent = '<p><i class="fas fa-circle-info"></i> Horibyte Website Version 2.3.3.3096.main.250308-1010</p><i class="fas fa-user-gear"></i>&nbsp;' + userAgentString;
   document.getElementById('userAgent').innerHTML = htmlContent;
 }
 
