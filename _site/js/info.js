6e611ee5dd1ef1bc0e853bab996c04b4eacdafe2
Horibyte <thehoribyte@gmail.com>
2025-05-07 19:47:47 -0600

    2.5.3567

--- _site/js/info.js
+++ _site/js/info.js
@@ -2,7 +2,7 @@ var intervalId;
 
 function displayUserAgent() {
   var userAgentString = navigator.userAgent;
-  var originalText = '<p><i class="fas fa-circle-info"></i> HoriWebsite Version 2.4.3565.main.250406-1905</p><i class="fas fa-user-gear"></i>&nbsp;' + userAgentString;
+  var originalText = '<p><i class="fas fa-circle-info"></i> HoriWebsite Version 2.5.3567.main.255407-1947</p><i class="fas fa-user-gear"></i>&nbsp;' + userAgentString;
   var htmlContent = originalText; // Default to original text
 
   const isLocalhost = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost";
